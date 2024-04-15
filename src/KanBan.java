65470f0949711410f385f03f9cf1fec6d2518be5
Lesego Theo Hans-Junior Nier Sindani <ST10443362@vcconnect.edu.za>
2024-04-15 14:57:09 +0200

    Boring

---
+++ src/KanBan.java
@@ -0,0 +1,35 @@
+
+
+public class KanBan {
+
+    private static String password;
+    private static String username;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the KanBan App");
+
+        Login kanBan = new Login();
+        String firstName = kanBan.promptFirstName();
+        String lastName = kanBan.promptLastName();
+        while (true) {
+            boolean isUserNameValid = kanBan.checkUserName();
+            boolean isPasswordComplex = kanBan.checkPasswordComplexity();
+            kanBan.registerUser(isUserNameValid, isPasswordComplex);
+            if (isUserNameValid && isPasswordComplex) {
+                break;
+            }
+        }
+
+        while (true) {
+            boolean isUserLoggedIn = kanBan.loginUser(username, password);
+
+
+        String loginStatus = kanBan.returnLoginStatus(isUserLoggedIn);
+        System.out.println(loginStatus);
+            if (isUserLoggedIn) {
+                break;
+            }
+
+        }
+    }
+}
