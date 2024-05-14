122d607efa2995e1158cdad6e5e8fdcd1648bd54
Lesego Sindani <st10443362@vcconnect.edu.za>
2024-05-14 22:22:47 +0200

    part 2 starting

--- src/KanBan.java
+++ src/KanBan.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+
 public class KanBan {
+    private static final List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to the KanBan App");
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Please Enter Your First Name");
+        /*System.out.println("Please Enter Your First Name");
         String firstName = userInput.nextLine();
         System.out.println("Please Enter Your Last Name");
         String lastName = userInput.nextLine();
@@ -14,15 +18,45 @@ public class KanBan {
         RegisterUser.registerUsername();
         RegisterUser.registerUserPassword();
 
-        boolean loginSuccess = LoginUser.login();
+
+        boolean loginSuccess;
+        do {
+            loginSuccess = LoginUser.login();
+            if (!loginSuccess) {
+                System.out.println("Login failed. Please try again.");
+            }
+        } while (!loginSuccess);
+        System.out.println("Welcome " + firstName + " " + lastName + ", it is great to see you again.");*/
+        boolean  loginSuccess = true;
 
         if (loginSuccess) {
-            System.out.println("Welcome " + firstName + " " + lastName + ", it is great to see you again.");
-        } else {
-            System.out.println("Login failed. Please try again.");
+            boolean quit = false;
+            while (!quit) {
+                System.out.println("1. Add task\n2. Display task details\n3. Quit application");
+                int choice = userInput.nextInt();
+                userInput.nextLine();
+                switch (choice) {
+                    case 1:
+                        Task.addTask(userInput);
+                        break;
+                    case 2:
+                        if (Task.getTasks().isEmpty()) {
+                            System.out.println("No tasks to display.");
+                        } else {
+                            Task.displayTaskDetails();
+                        }
+                        break;
+                    case 3:
+                        quit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            }
         }
-
     }
+
+
 }
 /*References listed below
 
