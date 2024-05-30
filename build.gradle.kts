92f64a9b9e97061c23738a2f56b1efa59deb9408
Lesego Sindani <st10443362@vcconnect.edu.za>
2024-05-30 22:22:16 +0200

    refactored test directory and files within

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,19 @@
  * This is a general purpose Gradle build.
  * To learn more about Gradle by exploring our Samples at https://docs.gradle.org/8.7/samples
  */
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
